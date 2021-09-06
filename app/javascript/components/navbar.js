28f663ed46ec7b73700e74c9fdae472965b6c32f
Jason Jun <uxd.jun@gmail.com>
2021-09-06 17:22:51 +1000

    Logo transition

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,11 +1,15 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
+  const whiteLogo = document.querySelector('.logo_wrapper img.white');
+
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-lewagon-white');
+        navbar.classList.add('navbar-lewagon-darkblue');
+        whiteLogo.classList.add('logo-visible');
       } else {
-        navbar.classList.remove('navbar-lewagon-white');
+        navbar.classList.remove('navbar-lewagon-darkblue');
+        whiteLogo.classList.remove('logo-visible');
       }
     });
   }
