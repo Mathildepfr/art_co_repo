57fb820f95cbe4fcf73f38aae0afa56f884d4f32
Jason Jun <uxd.jun@gmail.com>
2021-09-06 16:29:51 +1000

    Added transparent scrolling navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,9 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
 });
