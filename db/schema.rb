ad6664bc8a33f34f76d1fa78d2decc18747c8ec7
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-09-06 22:18:33 +1000

    photo url for atworks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_112905) do
+ActiveRecord::Schema.define(version: 2021_09_06_120820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_112905) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "medium"
+    t.string "photo_url"
     t.index ["collection_id"], name: "index_artworks_on_collection_id"
   end
 
