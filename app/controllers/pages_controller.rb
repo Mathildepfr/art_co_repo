c20aedd38cda8d82dcb1ff2f8582ae15dee7934e
Alastair Tooth <alastairtooth@gmail.com>
2021-09-13 11:20:08 +1000

    create search page in pages controller and integrate search function into navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,14 @@ class PagesController < ApplicationController
   def home
     @artworks = Artwork.all
   end
+
+  def search
+    if params[:query].present?
+      sql_query = "first_name ILIKE :query OR last_name ILIKE :query"
+      @users = User.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @users = User.all
+    end
+  end
+
 end
